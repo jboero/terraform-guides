154ec14802e49f7db9ea18b9fbc9e6c6945f70e9
JohnnyB <jboero@users.noreply.github.com>
2018-06-21 20:05:15 +0100

    Update main.tf

--- infrastructure-as-code/k8s-cluster-eks/deploy-pods/main.tf
+++ infrastructure-as-code/k8s-cluster-eks/deploy-pods/main.tf
@@ -1,6 +1,6 @@
 provider "kubernetes" {
-    host = "${data.terraform_remote_state.k8s_cluster.endpoint}"
-    cluster_ca_certificate = "${base64decode(data.terraform_remote_state.k8s_cluster.ca-cert)}"
+    host = "${data.terraform_remote_state.k8s_cluster.outputs.endpoint}"
+    cluster_ca_certificate = "${base64decode(data.terraform_remote_state.k8s_cluster.outputs.ca-cert)}"
 }
 
 data "terraform_remote_state" "k8s_cluster" {
